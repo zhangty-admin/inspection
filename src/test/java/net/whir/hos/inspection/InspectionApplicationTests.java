4e77369bf8b4d127d6c6083d717dc744a6706416
zhangty <sy829322@163.com>
2020-03-27 14:04:03 +0800

    初始化项目

---
+++ src/test/java/net/whir/hos/inspection/InspectionApplicationTests.java
@@ -0,0 +1,13 @@
+package net.whir.hos.inspection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InspectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
