7233f8860463a29b8eea15b34981ed736db8ede3
zhangty <sy829322@163.com>
2020-05-26 09:29:43 +0800

    修改多选巡检计划，消息提醒修改

---
+++ src/main/java/net/whir/hos/inspection/pc/bean/EmployeeInspectionIds.java
@@ -0,0 +1,30 @@
+package net.whir.hos.inspection.pc.bean;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * @Author: zty
+ * @Date: 2020/4/24 3:30 下午
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ApiModel(value = "人员和巡检计划ID")
+public class EmployeeInspectionIds {
+
+    @ApiModelProperty(value = "人员")
+    private Employee employee;
+
+    @ApiModelProperty(value = "巡检计划ID")
+    private List<Long> insIds;
+
+}
---
+++ src/main/java/net/whir/hos/inspection/pc/dao/InspectionEmployeeDao.java
@@ -0,0 +1,11 @@
+package net.whir.hos.inspection.pc.dao;
+
+import net.whir.hos.inspection.pc.bean.EmployeeIdInspectionId;
+import tk.mybatis.mapper.common.Mapper;
+
+/**
+ * @Author: zty
+ * @Date: 2020/5/25 2:22 下午
+ */
+public interface InspectionEmployeeDao extends Mapper<EmployeeIdInspectionId> {
+}
